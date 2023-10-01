0c160125cd5dfbf2b58280bf44e164b391129fe2
Vladimir Lazić <contact@vlazic.com>
2023-10-01 21:00:10 +0200

    Refactoring terrainColor Function to Accept User-Provided Terrain Configurations

--- engine/TerrainColor.js
+++ engine/TerrainColor.js
@@ -1,66 +1,84 @@
-// soon to be modular and user defined. Users will be able to adjust values and create their own worlds.
+/**
+ * Determines the appropriate terrain color based on the value of c.
+ *
+ * @param {number} c - The terrain coefficient, typically between 0 and 1.
+ * @param {object|string} [terrainConfig=defaultTerrains] - The terrain configuration object or its JSON representation.
+ * @returns {string} - The RGB color value for the terrain.
+ */
+function terrainColor(c, terrainConfig = defaultTerrains) {
+    let terrains;
 
-function terrainColor(c) {
-    // sea
-    if (c < .33) {
-        if (c<.055) {
-            return `rgb(0, 15, 44)`
-        } else if (c<.110) {
-            return `rgb(0, 31, 60)`
-        } else if (c<.165) {
-            return `rgb(3, 55, 81)`
-        } else if (c<.220) {
-            return `rgb(14, 78, 106)`
-        } else if (c<.275) {
-            return `rgb(23, 93, 119)`
-        } else {
-            return `rgb(37, 107, 133)`
+    // Check if the provided config is JSON, if so, parse it.
+    if (typeof terrainConfig === "string") {
+        try {
+            terrains = JSON.parse(terrainConfig);
+        } catch (e) {
+            throw new Error("Invalid JSON provided for terrainConfig");
         }
+    } else if (typeof terrainConfig === "object") {
+        terrains = terrainConfig;
+    } else {
+        throw new Error("Invalid terrainConfig provided. Expected JSON string or object.");
     }
-    // beach
-    else if (c < .5) {
-        if (c<.358) {
-            return `rgb(100, 171, 227)`
-        } else if (c<.386) {
-            return `rgb(146, 196, 238)`
-        } else if (c<.414) {
-            return `rgb(187, 219, 247)`
-        } else if (c<.442) {
-            return `rgb(246, 227, 212)`
-        } else if (c<.47) {
-            return `rgb(253, 216, 181)`
-        } else {
-            return `rgb(249, 209, 153)`
-        }
-    }
-    // plains
-    else if (c < .75) {
-        if (c<.54) {
-            return `rgb(198, 204, 81)`
-        } else if (c<.58) {
-            return `rgb(151, 193, 75)`
-        } else if (c<.62) {
-            return `rgb(128, 177, 69)`
-        } else if (c<.66) {
-            return `rgb(89, 149, 74)`
-        } else if (c<.70) {
-            return `rgb(68, 119, 65)`
-        } else {
-            return `rgb(49, 96, 51)`
-        }
-    }
-    // mountains
-    else {
-        if (c<.80) {
-            return `rgb(111, 65, 23)`
-        } else if (c<.85) {
-            return `rgb(57, 29, 0)`
-        } else if (c<.90) {
-            return `rgb(98, 91, 91)`
-        } else if (c<.95) {
-            return `rgb(79, 69, 73)`
-        } else {
-            return `rgb(62, 55, 62)`
+
+    for (let terrain of terrains) {
+        if (c >= terrain.range[0] && c < terrain.range[1]) {
+            for (let colorStop of terrain.colors) {
+                if (c < colorStop.limit) {
+                    return colorStop.value;
+                }
+            }
         }
     }
 }
+
+const defaultTerrains = [
+    {
+        name: "sea",
+        range: [0, 0.33],
+        colors: [
+            { limit: 0.055, value: 'rgb(0, 15, 44)' },
+            { limit: 0.110, value: 'rgb(0, 31, 60)' },
+            { limit: 0.165, value: 'rgb(3, 55, 81)' },
+            { limit: 0.220, value: 'rgb(14, 78, 106)' },
+            { limit: 0.275, value: 'rgb(23, 93, 119)' },
+            { limit: 0.33, value: 'rgb(37, 107, 133)' }
+        ]
+    },
+    {
+        name: "beach",
+        range: [0.33, 0.5],
+        colors: [
+            { limit: 0.358, value: 'rgb(100, 171, 227)' },
+            { limit: 0.386, value: 'rgb(146, 196, 238)' },
+            { limit: 0.414, value: 'rgb(187, 219, 247)' },
+            { limit: 0.442, value: 'rgb(246, 227, 212)' },
+            { limit: 0.47, value: 'rgb(253, 216, 181)' },
+            { limit: 0.5, value: 'rgb(249, 209, 153)' }
+        ]
+    },
+    {
+        name: "plains",
+        range: [0.5, 0.75],
+        colors: [
+            { limit: 0.54, value: 'rgb(198, 204, 81)' },
+            { limit: 0.58, value: 'rgb(151, 193, 75)' },
+            { limit: 0.62, value: 'rgb(128, 177, 69)' },
+            { limit: 0.66, value: 'rgb(89, 149, 74)' },
+            { limit: 0.70, value: 'rgb(68, 119, 65)' },
+            { limit: 0.75, value: 'rgb(49, 96, 51)' }
+        ]
+    },
+    {
+        name: "mountains",
+        range: [0.75, 1],
+        colors: [
+            { limit: 0.80, value: 'rgb(111, 65, 23)' },
+            { limit: 0.85, value: 'rgb(57, 29, 0)' },
+            { limit: 0.90, value: 'rgb(98, 91, 91)' },
+            { limit: 0.95, value: 'rgb(79, 69, 73)' },
+            { limit: 1, value: 'rgb(62, 55, 62)' }
+        ]
+    }
+];
+
