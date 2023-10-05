609717ddef49319cfe8c1b73ad73f02c6c19d39c
Khenzii <khenzii.was.taken@gmail.com>
2023-10-05 17:28:41 +0200

    Added the `help` command :)

--- engine/Commands.js
+++ engine/Commands.js
@@ -1,6 +1,9 @@
 // variable for if the command box is focused
 let isfocused = false;
 
+// variable for if we should fade out the result element
+let fadeout = true;
+
 // enter the command box
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Enter' && !isfocused) {
@@ -66,7 +69,7 @@ document.getElementById('commandbox').addEventListener('keyup', function (e) {
                 document.getElementById("coordinates").innerHTML = "Coordinates: X=" + posX/10 + ", Y=" + (posY/10)*-1;
             }
         }
-        // teleport to coordinates in the current world
+        // change the color of the world
         else if (command.toUpperCase() === "COLOR") {
             if (color === 0) {
                 color = 1;
@@ -79,6 +82,60 @@ document.getElementById('commandbox').addEventListener('keyup', function (e) {
             }
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             draw();
+        }
+
+        // list avaible commands
+        else if (command.toUpperCase().startsWith("HELP")) {
+            // make the box larger (temporarily)
+            document.getElementById('result').style.height = "240px";
+
+            // give the element some more time
+            fadeout = false
+
+            // check if the command was ran alone (just `help`)
+            // or if user requested details about a certain one
+            if(command.toUpperCase() === "HELP") {
+                document.getElementById('result').value = `:) command list: \n 1. color \n 2. quality \n 3. seed \n 4. speed \n 5. teleport/tp \n\nYou can get more info on a speciffic one by using 'help <command_name>'`;
+            } else {
+                // get the command that the user wants to get info on
+                const args = command.split(" ")
+                var command_name = args[args.length - 1] // get the last command specified
+
+                switch(command_name.toUpperCase()) {
+                    case "COLOR":
+                        document.getElementById('result').style.height = "75px";
+                        document.getElementById('result').value = `Toggle between full world color and gray scale to view underlying perlin plane`
+                        break;
+                    case "QUALITY":
+                        document.getElementById('result').style.height = "75px";
+                        document.getElementById('result').value = `Set rendering quality, the lower the more detailed. Anything below 10 will lag. Range 5-20`
+                        break;
+                    case "SEED":
+                        document.getElementById('result').style.height = "125px";
+                        document.getElementById('result').value = `Generate a new world with the given seed. Any length of characters, words, or numbers can be entered for a unique world`
+                        break;
+                    case "SPEED":
+                        document.getElementById('result').style.height = "25px";
+                        document.getElementById('result').value = `Set flight speed. Range 1-10`
+                        break;
+                    case "TELEPORT":
+                        document.getElementById('result').style.height = "50px";
+                        document.getElementById('result').value = `Teleport to specified coordinates`
+                        break;
+                    case "TP":
+                        document.getElementById('result').style.height = "50px";
+                        document.getElementById('result').value = `Teleport to specified coordinates`
+                        break;
+                    default:
+                        document.getElementById('result').style.height = "";
+                        document.getElementById('result').value = `Invalid command.`
+                }
+            }
+
+            // after 5 seconds, let the message fade out
+            setTimeout(function() {
+                fadeout = true;
+            }, 5000);
         }
         // invalid command // command not found
         else {
@@ -105,13 +162,33 @@ document.getElementById('commandbox').addEventListener('blur', function() {
 function fadeOutEffect() {
     let fadeTarget = document.getElementById('result');
     fadeTarget.style.opacity = 1;
+
+    if(!fadeout) {
+        // check every 0.1 second
+        setTimeout(function() {
+            fadeOutEffect();
+        }, 100);
+
+        // don't downgrade the opacity
+        return;
+    }
+
     let fadeEffect = setInterval(function () {
+        if(!fadeout) {
+            return
+        }
+
         if (!fadeTarget.style.opacity) {
             fadeTarget.style.opacity = 1;
         }
         if (fadeTarget.style.opacity > 0) {
             fadeTarget.style.opacity -= 0.1;
         } else {
+            // if used the help command, reset the height
+            if(fadeTarget.style.height !== "25px") {
+                fadeTarget.style.height = "25px";
+            }
+
             clearInterval(fadeEffect);
         }
     }, 500);
