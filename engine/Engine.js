563607ad01e279c1b69eac34567b4e3197d77c11
richardhartleydev <richardhartleydev@gmail.com>
2023-10-05 12:37:29 +0100

    engine.js file

--- engine/Engine.js
+++ engine/Engine.js
@@ -1,13 +1,14 @@
-let canvas= document.getElementById("canvas");
+let canvas = document.getElementById("canvas");
 let rect = canvas.getBoundingClientRect();
-let width = canvas.width = rect.width;
-let height = canvas.height = rect.height;
+let width = (canvas.width = rect.width);
+let height = (canvas.height = rect.height);
 let ctx = canvas.getContext("2d");
 
 // position of current screen
 let posX = 0;
 let posY = 0;
-document.getElementById("coordinates").innerHTML = "Coordinates: X=" + posX + ", Y=" + posY;
+document.getElementById("coordinates").innerHTML =
+  "Coordinates: X=" + posX + ", Y=" + posY;
 
 // block size by pixel, i wouldn't recommend going under 5 load times will be longer
 let quality = 10;
@@ -21,7 +22,7 @@ let color = 1;
 document.getElementById("color").innerHTML = "Color: " + color;
 
 // start every game with random seed
-let seedVal = Math.floor(Math.random()*1000000);
+let seedVal = Math.floor(Math.random() * 1000000);
 seed(HashToNumber(SHA256(seedVal + "")));
 document.getElementById("seed").innerHTML = "Seed: " + seedVal;
 
@@ -36,24 +37,39 @@ document.addEventListener('wheel', function(e) {
         zoom+=100;
     }
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    draw();
-})
+    draw();w
+})ww
  */
 
+// Initial draw function
 function draw() {
-    for (let x = 0; x < width; x += quality) {
-        for (let y = 0; y < height; y += quality) {
-            // current perlin curve simulates sea level
-            if (color === 0) {
-                ctx.fillStyle = `rgba(0, 0, 0, ` + (perlin2((x + posX) / 300, (y + posY) / 300) + 1) / 2 + `)`;
-            } else {
-                let seaLevel = (perlin2((x+posX)/300,(y+posY)/300)+1)/2;
-                //let temperature = (perlin2((x+posX)/300,(y+posY)/300)+1)/.5;
-                //let humidity = (perlin2((x+posX)/300,(y+posY)/300)+1)/.5;
-                ctx.fillStyle = terrainColor(seaLevel);
-            }
-            ctx.fillRect(x, y, quality, quality);
-        }
+  for (let x = 0; x < width; x += quality) {
+    for (let y = 0; y < height; y += quality) {
+      const seaLevel = calculateSeaLevel(x, y);
+      ctx.fillStyle = getColor(seaLevel);
+      ctx.fillRect(x, y, quality, quality);
     }
+  }
 }
 
+// Baseline sea level
+function calculateSeaLevel(x, y) {
+  // set values to variables so they can be adjusted (by slider?)
+  let heightFromGround = 300; // nice visual range: 250 - 500
+  let mountainHeight = 1.0; // nice visual range: 0 - 1.3
+
+  return (
+    (perlin2((x + posX) / heightFromGround, (y + posY) / heightFromGround) +
+      mountainHeight) /
+    2
+  );
+}
+
+// Define terrain
+function getColor(seaLevel) {
+  if (color === 0) {
+    return `rgba(0, 0, 0, ${seaLevel})`;
+  } else {
+    return terrainColor(seaLevel);
+  }
+}
