91c2cf68e758379e19cd1dcbe2eedc68124fe770
Vladimir Lazić <contact@vlazic.com>
2023-10-05 17:38:24 +0200

    Introduce CommandHandler.js and enhance command functionality
    - Add CommandHandler.js for unified command operations.
    - Refactor Commands.js to only contain command definitions.
    - Implement multiline display for command results.
    - Add dynamic size calculation for command output display.
    - Centralize HTML element references in Engine.js for performance.
    - Refactor UI.js and encapsulate content in a class for readability.
    - Highlight potential issue with global variables in Engine.js.
    
    Note: Consider addressing global variable usage in future iterations.

--- engine/Commands.js
+++ engine/Commands.js
@@ -1,118 +1,96 @@
-// variable for if the command box is focused
-let isfocused = false;
-
-// enter the command box
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter' && !isfocused) {
-        isfocused = true;
-        document.getElementById('commandbox').focus();
-    }
+// Seed command - spawns the player into the starting position of a new world.
+const seedCommand = new Command("SEED", "SEED <seed_value>", 1, (args) => {
+  const newSeed = args[0];
+  seed(HashToNumber(SHA256(newSeed)));
+  posX = posY = 0;
+  $result.value = "seed set to `" + newSeed + "`";
+  $seed.innerHTML = "Seed: " + newSeed;
+  draw();
+  return "seed set to `" + newSeed + "`";
 });
 
-// list of commands
-document.getElementById('commandbox').addEventListener('keyup', function (e) {
-    if (e.key === 'Enter' && isfocused && document.getElementById('commandbox').value !== "") {
-        // retrieve string entered from command box
-        let command = document.getElementById('commandbox').value;
-
-        // modify seed command, spawns you into the starting position of a new world
-        if (command.toUpperCase().startsWith("SEED ")) {
-            let newSeed = command.substring(5);
-            seed(HashToNumber(SHA256(newSeed)));
-            posX = 0;
-            posY = 0;
-            document.getElementById('result').value = "seed set to `" + newSeed + "`";
-            document.getElementById("seed").innerHTML = "Seed: " + newSeed;
-            draw();
-        }
-        // adjust speed of movement
-        else if (command.toUpperCase().startsWith("SPEED")) {
-            const speedArr = command.split(" ");
-            if (speedArr.length !== 2 || isNaN(Number(speedArr[1])) || !Number.isInteger(Number(speedArr[1]))) {
-                document.getElementById('result').value = "Invalid syntax `speed <speed>`";
-            } else if (Number(speedArr[1]) > 10 || Number(speedArr[1]) < 1) {
-                document.getElementById('result').value = "Invalid input. Range 1-10";
-            } else {
-                speed = Number(speedArr[1]);
-                document.getElementById('result').value = "New speed set to " + speedArr[1];
-                document.getElementById("speed").innerHTML = "Speed: " + speed;
-            }
-        }
-        // adjust quality of the world
-        else if (command.toUpperCase().startsWith("QUALITY")) {
-            const qualArr = command.split(" ");
-            if (qualArr.length !== 2 || isNaN(Number(qualArr[1])) || !Number.isInteger(Number(qualArr[1]))) {
-                document.getElementById('result').value = "Invalid syntax `quality <quality>`";
-            } else if (Number(qualArr[1]) > 20 || Number(qualArr[1]) < 5) {
-                document.getElementById('result').value = "Invalid input. Range 5-20";
-            } else {
-                quality = Number(qualArr[1]);
-                draw();
-                document.getElementById('result').value = "Quality set to " + qualArr[1];
-                document.getElementById("quality").innerHTML = "Quality: " + quality + "px";
-            }
-        }
-        // teleport to coordinates in the current world
-        else if (command.toUpperCase().startsWith("TELEPORT") || command.toUpperCase().startsWith("TP")) {  // alias 'tp' and 'teleport'
-            const telArr= command.split(" ");
-            if (telArr.length !== 3 || isNaN(Number(telArr[1])) || !Number.isInteger(Number(telArr[1]))
-                || isNaN(Number(telArr[2])) || !Number.isInteger(Number(telArr[2]))) {
-                document.getElementById('result').value = "Invalid syntax `teleport <x> <y>`";
-            } else {
-                posX = Number(telArr[1])*10;
-                posY = (-1)*Number(telArr[2])*10;
-                draw();
-                document.getElementById('result').value = "Teleported to " + telArr[1] + " " + telArr[2];
-                document.getElementById("coordinates").innerHTML = "Coordinates: X=" + posX/10 + ", Y=" + (posY/10)*-1;
-            }
-        }
-        // teleport to coordinates in the current world
-        else if (command.toUpperCase() === "COLOR") {
-            if (color === 0) {
-                color = 1;
-                document.getElementById('result').value = "Color on.";
-                document.getElementById("color").innerHTML = "Color: " + color;
-            } else {
-                color = 0;
-                document.getElementById('result').value = "Color off.";
-                document.getElementById("color").innerHTML = "Color: " + color;
-            }
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            draw();
-        }
-        // invalid command // command not found
-        else {
-            document.getElementById('result').value = "Invalid command.";
-        }
-        fadeOutEffect()
-        document.getElementById('commandbox').blur();
-        document.getElementById('commandbox').value = "";
-        isfocused=false;
-    }
-    // escape command focus
-    if (e.key === 'Esc' || e.key === 'Escape') {
-        document.getElementById('commandbox').blur();
-        document.getElementById('commandbox').value = "";
-        isfocused=false;
-    }
+// Speed command - adjusts the speed of movement.
+const speedCommand = new Command("SPEED", "SPEED <value>", 1, (args) => {
+  const speedVal = Number(args[0]);
+  if (isNaN(speedVal) || !Number.isInteger(speedVal)) {
+    return "Invalid syntax `speed <speed>`";
+  } else if (speedVal > 10 || speedVal < 1) {
+    return "Invalid input. Range 1-10";
+  } else {
+    speed = speedVal;
+    $speed.innerHTML = "Speed: " + speed;
+    return "New speed set to " + speedVal;
+  }
 });
 
-document.getElementById('commandbox').addEventListener('blur', function() {
-    isfocused = false;
-})
+// Quality command - adjusts the quality of the world.
+const qualityCommand = new Command("QUALITY", "QUALITY <value>", 1, (args) => {
+  const qualVal = Number(args[0]);
+  if (isNaN(qualVal) || !Number.isInteger(qualVal)) {
+    return "Invalid syntax `quality <quality>`";
+  } else if (qualVal > 20 || qualVal < 5) {
+    return "Invalid input. Range 5-20";
+  } else {
+    quality = qualVal;
+    draw();
+    $quality.innerHTML = "Quality: " + quality + "px";
+    return "Quality set to " + qualVal;
+  }
+});
 
-// fade out for result of command
-function fadeOutEffect() {
-    let fadeTarget = document.getElementById('result');
-    fadeTarget.style.opacity = 1;
-    let fadeEffect = setInterval(function () {
-        if (!fadeTarget.style.opacity) {
-            fadeTarget.style.opacity = 1;
-        }
-        if (fadeTarget.style.opacity > 0) {
-            fadeTarget.style.opacity -= 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 500);
-}
+// Teleport command - teleports the player to specific coordinates in the current world.
+const teleportCommand = new Command(
+  "TELEPORT",
+  "TELEPORT <x> <y> or TP <x> <y>",
+  2,
+  (args) => {
+    const x = Number(args[0]);
+    const y = Number(args[1]);
+    if (isNaN(x) || !Number.isInteger(x) || isNaN(y) || !Number.isInteger(y)) {
+      return "Invalid syntax `teleport <x> <y>`";
+    } else {
+      posX = x * 10;
+      posY = -1 * y * 10;
+      draw();
+      $coordinates.innerHTML =
+        "Coordinates: X=" + posX / 10 + ", Y=" + (posY / 10) * -1;
+      return "Teleported to " + x + " " + y;
+    }
+  },
+);
+
+// Color command - toggles the color setting.
+const colorCommand = new Command("COLOR", "COLOR", 0, (args) => {
+  const $color = $color;
+
+  if (color === 0) {
+    color = 1;
+    $color.innerHTML = "Color: " + color;
+    ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+    draw();
+    return "Color on.";
+  } else {
+    color = 0;
+    $color.innerHTML = "Color: " + color;
+    ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+    draw();
+    return "Color off.";
+  }
+});
+
+const helpCommand = new Command("HELP", "HELP", 0, (args, context) => {
+  let helpMessages = [];
+  context.commands.forEach((cmd) => {
+    helpMessages.push(`${cmd.name}: ${cmd.description}`);
+  });
+  return helpMessages.join("\n");
+});
+
+// Instantiate a new CommandHandler and register the commands.
+const handler = new CommandHandler();
+handler.register(seedCommand);
+handler.register(speedCommand);
+handler.register(qualityCommand);
+handler.register(teleportCommand);
+handler.register(colorCommand);
+handler.register(helpCommand);
--- engine/Controls.js
+++ engine/Controls.js
@@ -4,97 +4,98 @@ let keysPressed = {};
 // Flag to check if the sprint mode is active
 let sprinting = false;
 
-// HTML element to display current coordinates
-let coords = document.getElementById('coords');
-
 // Flag to determine if the canvas needs a redraw
 let needsRedraw = false;
 
 // Defining constants for keys to improve readability
 const KEYS = {
-    W: 'w',
-    A: 'a',
-    S: 's',
-    D: 'd',
-    ARROW_UP: 'ArrowUp',
-    ARROW_DOWN: 'ArrowDown',
-    ARROW_LEFT: 'ArrowLeft',
-    ARROW_RIGHT: 'ArrowRight'
+  W: "w",
+  A: "a",
+  S: "s",
+  D: "d",
+  ARROW_UP: "ArrowUp",
+  ARROW_DOWN: "ArrowDown",
+  ARROW_LEFT: "ArrowLeft",
+  ARROW_RIGHT: "ArrowRight",
 };
 
 // Registering keypress and release events
-document.addEventListener('keydown', registerKeyPress);
-document.addEventListener('keyup', deregisterKeyPress);
+document.addEventListener("keydown", registerKeyPress);
+document.addEventListener("keyup", deregisterKeyPress);
 
 // Handler for key press events
 function registerKeyPress(e) {
-    if (isfocused) return; // Makes sure commands aren't being typed
-    keysPressed[formatKey(e.key)] = true;
+  if (handler.isFocused) return; // Makes sure commands aren't being typed
+  keysPressed[formatKey(e.key)] = true;
 }
 
 // Handler for key release events
 function deregisterKeyPress(e) {
-    keysPressed[formatKey(e.key)] = false;
+  keysPressed[formatKey(e.key)] = false;
 }
 
 function formatKey(key) {
-    return key.startsWith('Arrow') ? key : key.toLowerCase();
+  return key.startsWith("Arrow") ? key : key.toLowerCase();
 }
 
 // Movement and rotation function
 function moveAndRotate(dx, dy, angle) {
-    posX += dx * speed * 10;
-    posY += dy * speed * 10;
-    document.getElementById("coordinates").innerHTML = "Coordinates: X=" + posX/10 + ", Y=" + (posY/10)*-1;
+  posX += dx * speed * 10;
+  posY += dy * speed * 10;
+  $coordinates.innerHTML =
+    "Coordinates: X=" + posX / 10 + ", Y=" + (posY / 10) * -1;
 
-    plane.rotate(angle);
+  plane.rotate(angle);
 
-    // Set the flag to redraw the canvas
-    needsRedraw = true;
+  // Set the flag to redraw the canvas
+  needsRedraw = true;
 }
 
-
 const movementMapping = {
-    [KEYS.W]: () => moveAndRotate(0, -1, 0),
-    [KEYS.ARROW_UP]: () => moveAndRotate(0, -1, 0),
-    [KEYS.A]: () => moveAndRotate(-1, 0, (3 * Math.PI) / 2),
-    [KEYS.ARROW_LEFT]: () => moveAndRotate(-1, 0, (3 * Math.PI) / 2),
-    [KEYS.S]: () => moveAndRotate(0, 1, Math.PI),
-    [KEYS.ARROW_DOWN]: () => moveAndRotate(0, 1, Math.PI),
-    [KEYS.D]: () => moveAndRotate(1, 0, Math.PI / 2),
-    [KEYS.ARROW_RIGHT]: () => moveAndRotate(1, 0, Math.PI / 2),
-    // Diagonal movements
-    [`${KEYS.W}-${KEYS.A}`]: () => moveAndRotate(0, 0, (7 * Math.PI) / 4),
-    [`${KEYS.ARROW_UP}-${KEYS.ARROW_LEFT}`]: () => moveAndRotate(0, 0, (7 * Math.PI) / 4),
-    [`${KEYS.W}-${KEYS.D}`]: () => moveAndRotate(0, 0, Math.PI / 4),
-    [`${KEYS.ARROW_UP}-${KEYS.ARROW_RIGHT}`]: () => moveAndRotate(0, 0, Math.PI / 4),
-    [`${KEYS.D}-${KEYS.S}`]: () => moveAndRotate(0, 0, (3 * Math.PI) / 4),
-    [`${KEYS.ARROW_RIGHT}-${KEYS.ARROW_DOWN}`]: () => moveAndRotate(0, 0, (3 * Math.PI) / 4),
-    [`${KEYS.A}-${KEYS.S}`]: () => moveAndRotate(0, 0, (5 * Math.PI) / 4),
-    [`${KEYS.ARROW_LEFT}-${KEYS.ARROW_DOWN}`]: () => moveAndRotate(0, 0, (5 * Math.PI) / 4)
+  [KEYS.W]: () => moveAndRotate(0, -1, 0),
+  [KEYS.ARROW_UP]: () => moveAndRotate(0, -1, 0),
+  [KEYS.A]: () => moveAndRotate(-1, 0, (3 * Math.PI) / 2),
+  [KEYS.ARROW_LEFT]: () => moveAndRotate(-1, 0, (3 * Math.PI) / 2),
+  [KEYS.S]: () => moveAndRotate(0, 1, Math.PI),
+  [KEYS.ARROW_DOWN]: () => moveAndRotate(0, 1, Math.PI),
+  [KEYS.D]: () => moveAndRotate(1, 0, Math.PI / 2),
+  [KEYS.ARROW_RIGHT]: () => moveAndRotate(1, 0, Math.PI / 2),
+  // Diagonal movements
+  [`${KEYS.W}-${KEYS.A}`]: () => moveAndRotate(0, 0, (7 * Math.PI) / 4),
+  [`${KEYS.ARROW_UP}-${KEYS.ARROW_LEFT}`]: () =>
+    moveAndRotate(0, 0, (7 * Math.PI) / 4),
+  [`${KEYS.W}-${KEYS.D}`]: () => moveAndRotate(0, 0, Math.PI / 4),
+  [`${KEYS.ARROW_UP}-${KEYS.ARROW_RIGHT}`]: () =>
+    moveAndRotate(0, 0, Math.PI / 4),
+  [`${KEYS.D}-${KEYS.S}`]: () => moveAndRotate(0, 0, (3 * Math.PI) / 4),
+  [`${KEYS.ARROW_RIGHT}-${KEYS.ARROW_DOWN}`]: () =>
+    moveAndRotate(0, 0, (3 * Math.PI) / 4),
+  [`${KEYS.A}-${KEYS.S}`]: () => moveAndRotate(0, 0, (5 * Math.PI) / 4),
+  [`${KEYS.ARROW_LEFT}-${KEYS.ARROW_DOWN}`]: () =>
+    moveAndRotate(0, 0, (5 * Math.PI) / 4),
 };
 
 // Game loop to handle movement and rendering
 function gameLoop() {
-    // Handle all movements: standard and diagonal
-    for (let keyCombination in movementMapping) {
-        const keys = keyCombination.split('-');
-        if (keys.every(key => keysPressed[key])) {
-            movementMapping[keyCombination]();
-        }
+  // Handle all movements: standard and diagonal
+  for (let keyCombination in movementMapping) {
+    const keys = keyCombination.split("-");
+    if (keys.every((key) => keysPressed[key])) {
+      movementMapping[keyCombination]();
     }
+  }
 
-    // Update displayed coordinates
-    coords.innerHTML = `X = ${posX / 10} Y = ${(-1) * posY / 10}`;
+  // Update displayed coordinates
+  $coords.innerHTML = `X = ${posX / 10} Y = ${(-1 * posY) / 10}`;
 
-    // Redraw the canvas if needed
-    if (needsRedraw) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw();
-        needsRedraw = false; // Reset the flag after drawing
-    }
+  // Redraw the canvas if needed
+  if (needsRedraw) {
+    ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+    draw();
+    needsRedraw = false; // Reset the flag after drawing
+  }
 
-    requestAnimationFrame(gameLoop); // Queue the next iteration
+  requestAnimationFrame(gameLoop); // Queue the next iteration
 }
 
 gameLoop(); // Initiate the game loop
